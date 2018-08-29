727feb19f3ad645ca251faf8f8b8840ae1c17ffe
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2018-08-29 10:21:05 +0300

    Chrome stable

--- all_setup_skykit.sh
+++ all_setup_skykit.sh
@@ -46,6 +46,11 @@ if [ "$userResponse" = 'yes' ]; then
 	sudo snap install mailspring
 	sudo snap install chromium
 	sudo apt install klavaro
+
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+	sudo apt-get update
+	sudo apt-get install google-chrome-stable
 fi
 
 read -p "Continue with GIT configuration? (yes/no)" userResponse
