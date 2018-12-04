5a012c35dc89497d9738e24e8fd8307367154eb1
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2018-12-04 16:53:48 +0200

    Added function to open a note with xmind

--- functii_notes.sh
+++ functii_notes.sh
@@ -20,4 +20,12 @@ function no() {
 	nvim ~/Documents/notes/$files 
 }
 
+function degandit() {
+	firefox "https://drive.google.com/drive/u/1/folders/1QSa3CpWR3RTAAXPTbnv5vAvd-rdy3sTi"
+}
+
+function xmind() {
+	/home/cristi/Downloads/xmind-8-update8-linux/XMind_amd64/XMind ~/Documents/notes/"$1".xmind
+}
+
 ########################################
