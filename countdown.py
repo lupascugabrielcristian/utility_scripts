d9be00acf97210bd9512c9f78ff6b680c64decf7
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2019-01-11 13:59:23 +0200

    Added countdown

---
+++ countdown.py
@@ -0,0 +1,46 @@
+import time
+from datetime import datetime
+
+
+tasksFileName = 'tasks'
+separator = '#'
+
+
+class CountdownTask:
+    name = "Undefined name"
+    endHour = 0
+    endMinute = 0
+
+def extractTaskFromLine(line):
+    parts = line.split(separator)
+    task = CountdownTask()
+    task.name = parts[0]
+    task.endHour = int(parts[1])
+    task.endMinute = int(parts[2])
+    return task
+
+def printTask(task):
+    taskEndInMinutes = task.endHour * 60 + task.endMinute
+    nowInMinutes = datetime.now().hour * 60 + datetime.now().minute
+    remaining = taskEndInMinutes - nowInMinutes
+    if remaining < 0:
+        print("[%s] END" % task.name)
+    else:
+        print("[%s] Ends in %d" % (task.name, remaining))
+
+
+
+def readFile():
+    while(1):
+        with open(tasksFileName) as tasksFile:
+            content = tasksFile.readlines()
+
+        tasks = list(map(lambda contentLine: extractTaskFromLine(contentLine), content))
+        print("\n\nThere are " + str(len(tasks)) + " tasks")
+        for t in tasks:
+            printTask(t)
+        time.sleep(10)
+
+
+
+readFile()
