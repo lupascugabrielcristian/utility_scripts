2b1c89800d9b3c6bcab0b05a2845fb6bdb9031b0
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2020-05-09 18:08:43 +0300

    New script pull from backup data server is working

--- sync_backup_data.sh
+++ sync_backup_data.sh
@@ -38,35 +38,31 @@ if [ "$operation" = 'h' -o "$operation" = 'push' -o "$operation" = 'Push' ]; the
 fi
 
 if [ "$operation" = 'l' -o "$operation" = 'pull' -o "$operation" = 'Pull' ]; then
-        printf "[+] Pulling from backup server\n"
+		printf "[+] Pulling from backup server\n"
         printf "[+] Getting keep file\n"
         rsync -rzv -e 'ssh -p 8522' --progress \
-                root@104.248.252.160:./sync_folder/keep.com \  
-                $home_folder/. #"$@" > /dev/null 
+                root@104.248.252.160:./sync_folder/keep.com  $home_folder/. "$@" > /dev/null
+ 
 
 
         printf "[+] Pulling vimwiki\n"
         rsync -rzv -e 'ssh -p 8522' --progress \
-                root@104.248.252.160:./sync_folder/vimwiki/ \  
-                $home_folder/vimwiki #"$@" > /dev/null 
+                root@104.248.252.160:./sync_folder/vimwiki/ $home_folder/vimwiki  "$@" > /dev/null 
 
 
         printf "[+] Pulling tools folder\n"
         rsync -rzv -e 'ssh -p 8522' --progress \
-                root@104.248.252.160:./sync_folder/tools/ \ 
-                $home_folder/Documents/tools #"$@" > /dev/null 
+                root@104.248.252.160:./sync_folder/tools/ $home_folder/Documents/tools "$@" > /dev/null 
 
 
         printf "[+] Pulling research folder\n"
         rsync -rzv -e 'ssh -p 8522' --progress \
-                root@104.248.252.160:./sync_folder/research/ \ 
-                $home_folder/Documents/research #"$@" > /dev/null 
-		
+                root@104.248.252.160:./sync_folder/research/ $home_folder/Documents/research "$@" > /dev/null 
+                
 
         printf "[+] Getting buku file\n"
         rsync -rzv -e 'ssh -p 8522' --progress \
-                root@104.248.252.160:./sync_folder/buku_backup.db \  
-                $home_folder/. #"$@" > /dev/null 
-		buku -i buku_backup.db
-		rm buku_backup.db
+                root@104.248.252.160:./sync_folder/buku_backup.db $home_folder/. "$@" > /dev/null 
+        buku -i $home_folder/buku_backup.db
+        rm $home_folder/buku_backup.db "$@" > /dev/null 
 fi
