dac62fbe827f02c67bc2d0da78111b46e83de764
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2020-08-13 14:05:02 +0300

    Firefox is opening only in specified tag

--- configurations/rc.lua
+++ configurations/rc.lua
@@ -529,8 +529,8 @@ awful.rules.rules = {
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox" },
+      properties = { tag = "6-browser" } },
 }
 -- }}}
 
