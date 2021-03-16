d8a3de8ec667b03c1e9ceda1ae6876c3b8ddc5cd
Lupascu Gabriel Cristian <62387-PoncioValdivia@users.noreply.gitlab.com>
2021-03-16 13:27:07 +0200

    Am uitat sa decomentez unele linii

--- sync_backup_data.sh
+++ sync_backup_data.sh
@@ -25,50 +25,50 @@ if [ "$operation" = 'h' -o "$operation" = 'push' -o "$operation" = 'Push' ]; the
 			read "[X] Timpul gasit pe server difera de cel la care s-a facut push de pe calculatorul asta. Trebuie facut pull mai intai"
 		fi
 
-        #printf "[+] Pushing to backup server\n"
-        #printf "[+] Sending keep file\n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        $home_folder/keep.com \
-        #        root@104.248.252.160:./sync_folder  "$@" > /dev/null
+        printf "[+] Pushing to backup server\n"
+        printf "[+] Sending keep file\n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                $home_folder/keep.com \
+                root@104.248.252.160:./sync_folder  "$@" > /dev/null
 
 
-        #printf "[+] Sending vimwiki\n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        $home_folder/vimwiki/ \
-        #        --filter='merge ./sync-filter.txt' \
-        #        root@104.248.252.160:./sync_folder/vimwiki  "$@" > /dev/null 
+        printf "[+] Sending vimwiki\n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                $home_folder/vimwiki/ \
+                --filter='merge ./sync-filter.txt' \
+                root@104.248.252.160:./sync_folder/vimwiki  "$@" > /dev/null 
 
 
-        #printf "[+] Sending tools folder\n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        $home_folder/Documents/tools/ \
-        #        --filter='merge ./sync-filter.txt' \
-        #        root@104.248.252.160:./sync_folder/tools  "$@" > /dev/null 
+        printf "[+] Sending tools folder\n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                $home_folder/Documents/tools/ \
+                --filter='merge ./sync-filter.txt' \
+                root@104.248.252.160:./sync_folder/tools  "$@" > /dev/null 
 
 
-        #printf "[+] Sending research folder\n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        $home_folder/Documents/research/ \
-        #        --filter='merge ./sync-filter.txt' \
-        #        root@104.248.252.160:./sync_folder/research  "$@" > /dev/null 
+        printf "[+] Sending research folder\n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                $home_folder/Documents/research/ \
+                --filter='merge ./sync-filter.txt' \
+                root@104.248.252.160:./sync_folder/research  "$@" > /dev/null 
 
-		#buku -e $home_folder/buku_backup.db
-        #printf "[+] Sending buku \n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        $home_folder/buku_backup.db \
-        #        root@104.248.252.160:./sync_folder  "$@" > /dev/null
-		#rm $home_folder/buku_backup.db
+		buku -e $home_folder/buku_backup.db
+        printf "[+] Sending buku \n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                $home_folder/buku_backup.db \
+                root@104.248.252.160:./sync_folder  "$@" > /dev/null
+		rm $home_folder/buku_backup.db
 
 
-		##Updatez timpul local de push pentru verificare ultirioara cu serverul
-		#time=$(date +%s)
-		#echo $home_folder > last_push.log
-		#echo $time >> last_push.log
+		#Updatez timpul local de push pentru verificare ultirioara cu serverul
+		time=$(date +%s)
+		echo $home_folder > last_push.log
+		echo $time >> last_push.log
 
-        #printf "[+] Sending last_push.log file \n"
-        #rsync -rzv -e 'ssh -p 8522' --progress \
-        #        last_push.log \
-        #        root@104.248.252.160:./sync_folder  "$@" > /dev/null
+        printf "[+] Sending last_push.log file \n"
+        rsync -rzv -e 'ssh -p 8522' --progress \
+                last_push.log \
+                root@104.248.252.160:./sync_folder  "$@" > /dev/null
 fi
 
 if [ "$operation" = 'l' -o "$operation" = 'pull' -o "$operation" = 'Pull' ]; then
