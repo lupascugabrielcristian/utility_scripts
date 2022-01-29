13fe3f9b835215c33dffb611f9958cb66765dc6b
BoukabousVisanzay <9123f887-43d7-46e7-b59f-5bc61b8dbb73@anonaddy.me>
2022-01-29 14:19:20 +0200

    Am mapat PageUp, PageDown, F1, F2, F3 si F4

--- map_keys.sh
+++ map_keys.sh
@@ -2,6 +2,10 @@
 # xev
 xmodmap -e "keycode 107 = Insert" # F13
 
+xmodmap -e "keycode 232 = F1"
+xmodmap -e "keycode 233 = F2"
+xmodmap -e "keycode 128 = F3"
+xmodmap -e "keycode 212 = F4"
 xmodmap -e "keycode 237 = F5"
 xmodmap -e "keycode 238 = F6"
 xmodmap -e "keycode 173 = F7"
@@ -10,3 +14,6 @@ xmodmap -e "keycode 171 = F9"
 xmodmap -e "keycode 121 = F10"
 xmodmap -e "keycode 122 = F11"
 xmodmap -e "keycode 123 = F12"
+
+xmodmap -e "keycode 112 = Prior" 	# Page up
+xmodmap -e "keycode 117 = Next"		# Page down
