653076a273436e116f4265b43209df097bddff49
BoukabousVisanzay <9123f887-43d7-46e7-b59f-5bc61b8dbb73@anonaddy.me>
2022-05-04 17:20:29 +0300

    Fix ip_scan

--- ip_scan.sh
+++ ip_scan.sh
@@ -3,10 +3,16 @@
 # /tmp/ips este un fisier care contine o lista de ip-uri
 # fiecare linie contine un ip in format 192.168.1.226
 
-while IFS=read -r line; do
+while IFS= read -r line; do
 	# Printeaza cu variabila
 	# printf 'ping -c2 %s\n' "$line"
 
 	# Ping pentru fiecare IP
 	ping -c2 $line
+done < /tmp/ips
+
+
+while IFS= read -r line; do
+	# Ping pentru fiecare IP
+	sudo nmap -O $line
 done < /tmp/ips
