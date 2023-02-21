470eeeaf298fb942b496d4142702fd9c54e86c6d
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2023-02-21 20:14:58 +0200

    Opresc containerul dupa ce export

--- sync_backup_data_2.sh
+++ sync_backup_data_2.sh
@@ -5,8 +5,13 @@
 # Scriptul asta va fi executat de comanda ../bin/sync_backup
 # Scriptul asta vine dupa sync_backup_data.sh ca sa nu mai trimit date nefolositoare
 # 
+# Presupuneri:
+# 1. Containerul docker pentru aplicatia study este denumit: study-mysql
+# 2. Credentialele containerului docker de la aplicatia study
+# 
 # Usage:
 # ./sync_backup_data.sh
+# Portul si denumirile fisierelor de backup, declarate mai jos, trebuie updatate
 
 
 PORT=7131
@@ -43,6 +48,19 @@ docker exec study-mysql /usr/bin/mysqldump -u root --password=studymqsql mysql >
 
 # Trimit la server backup buku
 scp -P $PORT /tmp/$STUDY_BACKUP_NAME ramonbassecharcan@165.232.117.151:./storage
+
+# Opresc containerul numit study-mysql
+open_containers=$(docker ps | awk 'NR>1 { print $13}')
+
+for l in $open_containers
+do
+	# if [[ "$l" == "study-test" ]]; then
+	# 	printf "este $l\n"
+	# fi
+
+	# Varianta scurta la cea de mai sus
+	[ "$l" == "study-mysql" ] && docker container stop study-mysql
+done
 
 
 # 3. Updatez timpul local de push pentru verificare ultirioara cu serverul
