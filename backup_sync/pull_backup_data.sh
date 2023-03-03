c7b9ca0a1a1cfc8d219b9b210deb109629298b12
Gabi <PabonBecerra@yandex.com>
2023-03-03 18:38:29 +0100

    Lucru la scriptul de pull

--- backup_sync/pull_backup_data.sh
+++ backup_sync/pull_backup_data.sh
@@ -9,6 +9,7 @@
 # 
 PORT=7131
 BUKU_BACKUP_NAME=buku_backup.db
+STUDY_BACKUP_NAME=study_backup.sql
 
 ## 1. BUKU
 echo "Getting buku database"
@@ -17,6 +18,77 @@ if [[ -f /tmp/$BUKU_BACKUP_NAME ]]; then
 	rm /tmp/$BUKU_BACKUP_NAME
 fi
 
+# Copii de la server fisierul exportat de buku
+scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/$BUKU_BACKUP_NAME /tmp/
 
-scp ....
-Trebuie sa las posibilitatea sa ma loghez cu parola cu user non-root
+# Import fisierul de la server
+buku -i /tmp/$BUKU_BACKUP_NAME
+
+# Sterg fisierul de la server
+rm /tmp/$BUKU_BACKUP_NAME
+
+
+## 2. STUDY-APP DATABASE 
+
+# Usage:
+# is_installed="$(package_installed neovim)"
+# if [[ "$is_installed" == 0 ]]; then
+#         echo is not installed
+# fi
+package_installed() {
+	local output="$(dpkg -l "${1}" 2>&1)"
+	local search_part='no packages found matching'
+
+	if [[ "$output" == *"$search_part"* ]]; then
+			# not installed
+			echo 0
+	else
+			# installed
+			echo 1
+	fi
+}
+
+sync_study_app() {
+	echo "[+] docker installed..."
+	found=0
+
+	# Verific daca exista containerul denumit "study-mysql"
+	# awk '{print $NF}' printeaza ultimul cuvant din rand
+	for l in $(docker container ls -a | awk '{print $NF}')
+	do
+		if [[ "$l" =~ .*study-mysql.* ]];then
+			echo "container found..."
+			found=1
+		fi
+	done
+
+	if [[ $found -eq 1 ]];then 
+		echo "Starting container..."
+		docker container start study-mysql
+
+		# Astept sa se porneasa complet
+		sleep 2
+
+		# Daca exista deja fisierul exportat il sterg
+		if [ -f /tmp/$STUDY_BACKUP_NAME ];then
+			rm /tmp/$STUDY_BACKUP_NAME
+		fi
+
+		# Iau de pe sever exportul de backup
+		scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/$STUDY_BACKUP_NAME /tmp/
+
+		# Import in baza de date
+	else
+		echo "study-mysql docker container not found. Skipping study-app sync"
+	fi
+}
+
+is_installed="$(package_installed docker)"
+if [[ "$is_installed" == 0 ]];then
+	echo "Docker not installed. Skipping study-app database"
+else
+	sync_study_app
+fi
+
+
+
