67b4cdbb0b1121eb98a31d23b79023b665eab439
Gabi <PabonBecerra@yandex.com>
2023-03-04 18:31:50 +0100

    Lucru la protectie scriere peste versiune mai noua

--- backup_sync/pull_backup_data.sh
+++ backup_sync/pull_backup_data.sh
@@ -10,28 +10,13 @@
 # Atentie:
 # Daca e prima data cand folosesc local aplicatia study-app, trebuie mai intai sa rulez binarul dupa care pot face importul de mysql dump
 #
+# Assumtions:
+# - numele containerului de la aplicatia study-app este study-mysql
+# - credentialele containerului mysql de la aplicatia study-app
+#
 PORT=7131
 BUKU_BACKUP_NAME=buku_backup.db
 STUDY_BACKUP_NAME=study_backup.sql
-
-## 1. BUKU
-echo "Getting buku database"
-# Daca exista deja fisierul de la server, il sterg sa nu faca overwrite
-if [[ -f /tmp/$BUKU_BACKUP_NAME ]]; then
-	rm /tmp/$BUKU_BACKUP_NAME
-fi
-
-# Copii de la server fisierul exportat de buku
-scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/$BUKU_BACKUP_NAME /tmp/
-
-# Import fisierul de la server
-buku -i /tmp/$BUKU_BACKUP_NAME
-
-# Sterg fisierul de la server
-rm /tmp/$BUKU_BACKUP_NAME
-
-
-## 2. STUDY-APP DATABASE 
 
 # Description:
 # Functie care imi arata daca un packet este instalat local
@@ -57,6 +42,54 @@ package_installed() {
 			echo 1
 	fi
 }
+
+echo "Checking server time..."
+# Iau logul de pe server pentru a compara daca timpul de pe server este mai vechi decat
+scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/.update.log /tmp/
+
+mapfile -t arr < /tmp/.update.log
+server_time=${arr[1]} # time cand s-a facut ultimul push la server
+server_user=${arr[0]} # userul care a facut ultimul push
+
+mapfile -t larr < $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+local_time=${larr[1]} # time cand am facut ultima data push de aici
+local_user=${larr[0]} # userul local
+
+printf "Server time: %d\nLocal last push: %d\n" $server_time $local_time
+
+# Verific ca versiunea locala sa fie mai veche decat cea de pe server
+if [[ $server_time -lt $local_time ]];then
+	read -p "[!] Versiunea de pe server e mai veche decat cea locala. Continui? [y]" userResponse 
+	if [[ "$userResponse" != 'y' ]];then
+		exit 1
+	fi
+fi
+
+
+## 1. BUKU
+is_installed="$(package_installed buku)"
+if [[ "$is_installed" == 0 ]];then
+	echo "Buku not installed. Skipping buku update"
+else
+	echo "Getting buku database..."
+	# Daca exista deja fisierul de la server, il sterg sa nu faca overwrite
+	if [[ -f /tmp/$BUKU_BACKUP_NAME ]]; then
+		rm /tmp/$BUKU_BACKUP_NAME
+	fi
+
+	# Copii de la server fisierul exportat de buku
+	scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/$BUKU_BACKUP_NAME /tmp/
+
+	# Import fisierul de la server
+	buku -i /tmp/$BUKU_BACKUP_NAME
+
+	# Sterg fisierul de la server
+	rm /tmp/$BUKU_BACKUP_NAME
+fi
+
+
+
+## 2. STUDY-APP DATABASE 
 
 # Description:
 # Functie care o apelez dupa ce stiu ca este instalat docker
@@ -92,10 +125,18 @@ sync_study_app() {
 		fi
 
 		# Iau de pe sever exportul de backup
+		echo "Getting database dump from server..."
 		scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/$STUDY_BACKUP_NAME /tmp/
 
 		# Import sql dump in containerul cu baza de date
+		echo "Importing to local container..."
 		docker exec -i study-mysql sh -c 'exec mysql -uroot -pstudymqsql mysql' < /tmp/$STUDY_BACKUP_NAME
+
+		# Stopping container
+		read -p "Stop study-app container? [y]" userResponse
+		if [[ "$userResponse" = 'y' ]];then
+			docker container stop study-mysql
+		fi
 	else
 		echo "study-mysql docker container not found. Skipping study-app sync"
 	fi
@@ -110,4 +151,10 @@ else
 fi
 
 
+# 3. Updatez timpul local de pull
+time=$(date +%s)
+echo $USER > $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+echo $local_time >> $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+# Randul 3 arata cand s-a facut ultima data pull
+echo $time >> $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
 
--- backup_sync/push_backup_data.sh
+++ backup_sync/push_backup_data.sh
@@ -19,6 +19,39 @@ PORT=7131
 BUKU_BACKUP_NAME=buku_backup.db
 STUDY_BACKUP_NAME=study_backup.sql
 
+echo "Checking server time..."
+# Iau logul de pe server pentru a compara daca timpul de pe server este mai vechi decat
+scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/.update.log /tmp/
+
+mapfile -t arr < /tmp/.update.log
+server_time=${arr[1]} # time cand s-a facut ultimul push la server
+server_user=${arr[0]} # userul care a facut ultimul push
+
+if [[ -f $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log ]];then
+	mapfile -t larr < $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+	local_user=${larr[0]} # Randul 1 este userul local
+	local_time=${larr[1]} # Randul 2 contine timpul cand am facut ultima data push de aici
+	local_pull=${larr[2]} # Randul 3 contine timpul cand s-a facut pull ultima data
+else
+	local_user=$USER
+	local_time=0
+	local_pull=0
+fi
+
+# In cazul in care userul local difera de userul de pe sever,
+# Sa nu fac push daca nu am facut pull
+# Adica timpul de pull sa fie mai mare decat timpul de pe server
+if [[ $server_user != $local_user ]];then
+	printf "local_pull=%d, server_time=%d\n" $local_pull $server_time
+	if [[ $local_pull -lt $server_time ]];then
+		read -p "Trebuie facut pull mai intai. Continui? [y]" userResponse
+
+		if [[ "$userResponse" != 'y' ]];then
+			exit 1
+		fi
+	fi
+fi
+
 ## 1. BUKU
 echo "Sending buku database"
 # Daca exista deja fisierul exportat il sterg ca sa nu ma intrebe daca ii fac overwrite
@@ -66,5 +99,8 @@ done
 
 # 3. Updatez timpul local de push pentru verificare ultirioara cu serverul
 time=$(date +%s)
-echo $USER > $LOCATION_OF_UTILITIES_FOLDER/last_push.log
-echo $time >> $LOCATION_OF_UTILITIES_FOLDER/last_push.log
+echo $USER > $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+echo $time >> $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
+
+# Trimit pe server .update.log
+scp -P $PORT $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log ramonbassecharcan@165.232.117.151:./storage
