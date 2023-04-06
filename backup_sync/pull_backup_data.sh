27f6dc069799a79b564f689af0363ae458104a45
Lupascu Gabriel Cristian <lupascugabrielcristian@gmail.com>
2023-04-06 10:56:10 +0300

    pull

--- backup_sync/pull_backup_data.sh
+++ backup_sync/pull_backup_data.sh
@@ -164,8 +164,12 @@ else
 	sync_study_app
 fi
 
+## 3. NOTES
+scp -P $PORT ramonbassecharcan@165.232.117.151:./storage/notes.tar.gz /tmp/
+# TODO Aici trebuie facut cu rsync - nu pare ca inlocuieste fisierele din arhiva existente local
+tar czf /tmp/notes.tar.gz -C ~/Documents/notes/ .
 
-# 3. Updatez timpul local de pull
+# 4. Updatez timpul local de pull
 time=$(date +%s)
 echo $USER > $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
 echo $local_time >> $LOCATION_OF_UTILITIES_FOLDER/backup_sync/.update.log
---
+++ templates/template_hello_world.rb
@@ -0,0 +1,10 @@
+class Exercitiu
+
+  def hello
+    puts "hello"
+  end
+
+end
+
+e = Exercitiu.new
+e.hello()
